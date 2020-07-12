950c1e813190688d0b192938dcb23974c557cec4
Pariya P <pariya.poocharoen@campus.viu.edu>
2020-07-12 03:21:44 -0400

    updated part 2

--- Javascript-Challenge/static/js/app.js
+++ Javascript-Challenge/static/js/app.js
@@ -1,16 +1,27 @@
 // from data.js
 var tableData = data;
+
 // Select the button
-var filterButton = d3.selectAll("#filter-btn");
+var dateBtn = d3.select("#filter-date-btn");
+var cityBtn = d3.select("#filter-city-btn");
+var stateBtn = d3.select("#filter-state-btn");
+var countryBtn = d3.select("#filter-country-btn");
+var shapeBtn = d3.select("#filter-shape-btn");
+
 // Create event handlers
-// button.on("click", a);
-filterButton.on("click", runEnter);
+dateBtn.on("click", datetime);
+cityBtn.on("click", city);
+stateBtn.on("click", state);
+countryBtn.on("click", country);
+shapeBtn.on("click", shape);
+
 // *************** Filter by 'date/time' ***************
 // *****************************************************
 // The event handler function for the form
-function runEnter() {
+function datetime() {
     // prevent the form from refreshing the page
-    d3.event.preventDefault() 
+    // d3.event.preventDefault() 
+
     // Select input element 
     var inputElement = d3.select("#datetime");
     // Get the value of input element
@@ -37,23 +48,16 @@ function runEnter() {
 
 // *************** Filter by 'city' ********************
 // *****************************************************
-// The event handler function for the form
-function runEnter() {
-    // Select input element 
+function city() {
+
     var inputElement = d3.select("#city");
-    // Get the value of input element
+
     var inputValue = inputElement.property("value");
-    // console.log(inputElement);
-    // console.log(inputValue);
 
-    // Use filter() to filter only the user-input dates -- in input field
     var filteredCity = tableData.filter(c => c.city == inputValue);
-    // console.log(filterTable);
 
-    // Get a reference to the table body
     var tbody = d3.select("tbody");
 
-    // Create table and populate data only on 'input date'
     filteredCity.forEach((c) => {
         var row = tbody.append("tr");
         Object.entries(c).forEach(([key,value]) => {
@@ -65,23 +69,16 @@ function runEnter() {
 
 // *************** Filter by 'state' *******************
 // *****************************************************
-// The event handler function for the form
-function runEnter() {
-    // Select input element 
+function state() {
+
     var inputElement = d3.select("#state");
-    // Get the value of input element
+
     var inputValue = inputElement.property("value");
-    // console.log(inputElement);
-    // console.log(inputValue);
 
-    // Use filter() to filter only the user-input dates -- in input field
     var filteredState = tableData.filter(st => st.state == inputValue);
-    // console.log(filterTable);
 
-    // Get a reference to the table body
     var tbody = d3.select("tbody");
 
-    // Create table and populate data only on 'input date'
     filteredState.forEach((st) => {
         var row = tbody.append("tr");
         Object.entries(st).forEach(([key,value]) => {
@@ -93,23 +90,16 @@ function runEnter() {
 
 // *************** Filter by 'country' *****************
 // *****************************************************
-// The event handler function for the form
-function runEnter() {
-    // Select input element 
+function country() {
+
     var inputElement = d3.select("#country");
-    // Get the value of input element
+
     var inputValue = inputElement.property("value");
-    // console.log(inputElement);
-    // console.log(inputValue);
 
-    // Use filter() to filter only the user-input dates -- in input field
     var filteredCountry = tableData.filter(co => co.country == inputValue);
-    // console.log(filterTable);
 
-    // Get a reference to the table body
     var tbody = d3.select("tbody");
 
-    // Create table and populate data only on 'input date'
     filteredCountry.forEach((co) => {
         var row = tbody.append("tr");
         Object.entries(co).forEach(([key,value]) => {
@@ -121,23 +111,16 @@ function runEnter() {
 
 // *************** Filter by 'shape' ******************* 
 // *****************************************************
-// The event handler function for the form
-function runEnter() {
-    // Select input element 
+function shape() {
+
     var inputElement = d3.select("#ufo-shape");
-    // Get the value of input element
+
     var inputValue = inputElement.property("value");
-    // console.log(inputElement);
-    // console.log(inputValue);
 
-    // Use filter() to filter only the user-input dates -- in input field
     var filteredShape = tableData.filter(shp => shp.shape == inputValue);
-    // console.log(filterTable);
 
-    // Get a reference to the table body
     var tbody = d3.select("tbody");
 
-    // Create table and populate data only on 'input date'
     filteredShape.forEach((shp) => {
         var row = tbody.append("tr");
         Object.entries(shp).forEach(([key,value]) => {
@@ -147,15 +130,11 @@ function runEnter() {
     });
 };
 
-
 // *************** Clear input button ******************* 
 // *****************************************************
-// var clearButton = d3.select("#clear-btn");
-// // Clear button on click clears fields
-// clearButton.on('click', function () {
-
-//     // Keeps page from refreshing completely, only want the table to refresh
-//     d3.event.preventDefault();
-//     // Clears input fields
-//     clearEntries()
-// });
+var clearButton = d3.select("#clear-btn");
+// Clear button on click clears fields
+clearButton.on('click', function () {
+    // Clear filtered table data
+    d3.selectAll("td").remove();
+});
--- Javascript-Challenge/static/js/pariya_app.js
+++ Javascript-Challenge/static/js/pariya_app.js
@@ -1,6 +1,146 @@
 // from data.js
 var tableData = data;
 // Select the button
-var filterButton = d3.selectAll("#filter-btn");
+var filterButton = d3.select("#filter-btn");
 // Create event handlers
-filterButton.on("click", runEnter);
+filterButton.on("click", checkInput);
+
+// *****************************************************************
+// Check if input field is filled, if yes --> run following function
+function checkInput() {
+    
+    var datetimeInput = d3.select("#datetime").property("value");
+    var cityInput = d3.select("#city").property("value");
+    var stateInput = d3.select("#state").property("value");
+    var countryInput = d3.select("#country").property("value");
+    var shapeInput = d3.select("#ufo-shape").property("value");
+
+    if (datetimeInput !== "") {
+        datetime();
+    } else if (cityInput !== "") {
+        city();
+    } else if (stateInput !== "") {
+        state();
+    } else if (countryInput !== "") {
+        country();
+    } else {
+        shape();
+    };
+};
+
+// *************** Filter by 'date/time' ***************
+// *****************************************************
+// The event handler function for the form
+function datetime() {
+    // prevent the form from refreshing the page
+    // d3.event.preventDefault() 
+
+    // Select input element 
+    var inputElement = d3.select("#datetime");
+
+    // Get the value of input element
+    var inputValue = inputElement.property("value");
+
+    // console.log(inputElement);
+    // console.log(inputValue);
+
+    // Use filter() to filter only the user-input dates -- in input field
+    var filteredDate = tableData.filter(d => d.datetime == inputValue);
+    // console.log(filterTable);
+
+    // Get a reference to the table body
+    var tbody = d3.select("tbody");
+
+    // Create table and populate data only on 'input date'
+    filteredDate.forEach((d) => {
+        var row = tbody.append("tr");
+        Object.entries(d).forEach(([key,value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+};
+
+// *************** Filter by 'city' ********************
+// *****************************************************
+function city() {
+
+    var inputElement = d3.select("#city");
+    var inputValue = inputElement.property("value");
+    var filteredCity = tableData.filter(c => c.city == inputValue);
+    var tbody = d3.select("tbody");
+
+    filteredCity.forEach((c) => {
+        var row = tbody.append("tr");
+        Object.entries(c).forEach(([key,value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+};
+
+// *************** Filter by 'state' *******************
+// *****************************************************
+function state() {
+
+    var inputElement = d3.select("#state");
+    var inputValue = inputElement.property("value");
+    var filteredState = tableData.filter(st => st.state == inputValue);
+    var tbody = d3.select("tbody");
+
+    filteredState.forEach((st) => {
+        var row = tbody.append("tr");
+        Object.entries(st).forEach(([key,value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+};
+
+// *************** Filter by 'country' *****************
+// *****************************************************
+function country() {
+
+    var inputElement = d3.select("#country");
+    var inputValue = inputElement.property("value");
+    var filteredCountry = tableData.filter(co => co.country == inputValue);
+    var tbody = d3.select("tbody");
+
+    filteredCountry.forEach((co) => {
+        var row = tbody.append("tr");
+        Object.entries(co).forEach(([key,value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+};
+
+// *************** Filter by 'shape' ******************* 
+// *****************************************************
+function shape() {
+
+    var inputElement = d3.select("#ufo-shape");
+    var inputValue = inputElement.property("value");
+    var filteredShape = tableData.filter(shp => shp.shape == inputValue);
+    var tbody = d3.select("tbody");
+
+    filteredShape.forEach((shp) => {
+        var row = tbody.append("tr");
+        Object.entries(shp).forEach(([key,value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+};
+
+// *************** Clear input button ******************* 
+// *****************************************************
+var clearButton = d3.select("#clear-btn");
+// Clear button on click clears fields
+clearButton.on('click', function () {
+    // Clear filtered table data
+    d3.selectAll("td").remove();
+});
+
+
+
